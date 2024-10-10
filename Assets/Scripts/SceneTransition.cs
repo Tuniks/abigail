9252511ab7bac7493a596ffe15b2cb5a190b8302
Tuniks <tonikpa@gmail.com>
2024-10-10 11:48:40 -0400

    adds scene transitions between prototype scenes

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour{
+    public string sceneName = null;
+
+    private void OnTriggerEnter(Collider other){
+        if(sceneName == null) return;
+        if(other.gameObject.tag != "Player") return;
+
+        SceneManager.LoadScene(sceneName);
+    }
+}
