52ba82666732a829707024fef8a735fa588a9fe2
Tuniks <tonikpa@gmail.com>
2024-11-05 00:19:15 -0500

    changes how movement works

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,10 +13,27 @@ public class PlayerController : MonoBehaviour{
     }
 
     void Update(){
+        // Tank Controls
+        // float x = Input.GetAxis("Horizontal");
+        // float y = Input.GetAxis("Vertical");
+
+        // controller.SimpleMove(transform.forward * y * movementSpeed);
+        // transform.Rotate(transform.up, rotationSpeed * x * Time.deltaTime);
+
+        // Normal Controls
+
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
-        controller.SimpleMove(transform.forward * y * movementSpeed);
-        transform.Rotate(transform.up, rotationSpeed * x * Time.deltaTime);
+        Vector3 camFor = Camera.main.transform.forward;
+        camFor.y = 0;
+        camFor.Normalize();
+        Vector3 camRight = Camera.main.transform.right;
+        camRight.y = 0;
+        camRight.Normalize();
+
+        Vector3 dir = camFor * y + camRight * x;
+
+        controller.SimpleMove(dir * movementSpeed);
     }
 }
