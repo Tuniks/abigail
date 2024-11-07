9e57f845bf2f9c8d966acb428dbcf930170754be
Tuniks <tonikpa@gmail.com>
2024-11-06 19:12:35 -0500

    standardize prefabs, scene structures, npcs

---
+++ Assets/Scripts/BillboardSprite.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardSprite : MonoBehaviour{
+    void LateUpdate(){
+        transform.forward = Camera.main.transform.forward;
+    }
+}
