c58a190234671fb0663549bcf9c7e6d5108c14c5
Tuniks <tonikpa@gmail.com>
2024-12-12 22:12:52 -0500

    persistent player inventory

--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInventory : Inventory{
+    public static PlayerInventory Instance;
+    
     [Header("UI")]
     public PlayerUIManager ui; 
+    
+    void Awake(){
+        DontDestroyOnLoad(gameObject);
+    }
     
     protected override void Start(){
         base.Start();
         ui.SetInventoryUI();
-    }
-
-    void Update(){
-        
+        if(Instance == null){
+            Instance = this;
+        } else Destroy(gameObject);
     }
 }
