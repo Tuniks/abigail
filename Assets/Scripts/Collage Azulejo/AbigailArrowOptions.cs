57ee8cca264f4e34f4fdef948460601b42a8767d
IsabelleSmith21 <is2812@nyu.edu>
2025-02-09 23:52:20 -0500

    It works!

--- Assets/Scripts/Collage Azulejo/AbigailArrowOptions.cs
+++ Assets/Scripts/Collage Azulejo/AbigailArrowOptions.cs
@@ -4,109 +4,80 @@ using UnityEngine;
 
 public class AbigailArrowOptions : MonoBehaviour
 {
+    // Enum for states
+    private enum State { Choose, Leave }
+    private State currentState = State.Choose;
+
     // Public GameObjects
-    public GameObject arrow; // The arrow GameObject
-    public GameObject Claw;
-    public GameObject Garden;
-    public GameObject ClawManager;
-    public GameObject GardenManager;
+    public GameObject arrow;         // The arrow sprite
+    public GameObject option1;       // Option 1 (Claw)
+    public GameObject option2;       // Option 2 (Garden)
+    public GameObject option1Manager; // Manager for Option 1
+    public GameObject option2Manager; // Manager for Option 2
 
-    // Variables to keep track of the current selection
-    private int selection = 0; // 0 for Claw, 1 for Garden
-    private GameObject currentSelection;
+    // Variables to track the current selection
+    private int selection = 0; // 0 for Option 1, 1 for Option 2
 
-    // Movement speed for the arrow
-    public float arrowSpeed = 5f;
-
-    // Start is called before the first frame update
     void Start()
     {
-        // Initially, disable both managers
-        ClawManager.SetActive(false);
-        GardenManager.SetActive(false);
-        arrow.SetActive(true);
+        // Ensure we start in the "Choose" state
+        currentState = State.Choose;
 
-        // Set initial selection to Claw
-        currentSelection = Claw;
-        SetActiveManager(); // Set the appropriate manager active
-        UpdateArrowPosition(); // Set arrow to initial selection position
+        // Position the arrow at the first option
+        arrow.transform.position = option1.transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Handle movement and selection with WASD/Arrow keys and Enter
-        HandleArrowMovement();
-
-        // When the player presses Enter or E, confirm selection
-        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.E))
+        if (currentState == State.Choose)
         {
-            SelectCurrentOption();
+            HandleArrowMovement();
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                ConfirmSelection();
+            }
         }
     }
 
-    // Handle the arrow's movement with WASD or Arrow keys
+    // Handle arrow movement between options
     void HandleArrowMovement()
     {
-        // Cycle between Claw and Garden with Up/Down or W/S keys
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
-            selection = 0; // Select Claw
-            UpdateArrowPosition(); // Update arrow to Claw position
+            if (selection != 0) // Move to Option 1
+            {
+                selection = 0;
+                arrow.transform.position = option1.transform.position;
+            }
         }
         else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
-            selection = 1; // Select Garden
-            UpdateArrowPosition(); // Update arrow to Garden position
+            if (selection != 1) // Move to Option 2
+            {
+                selection = 1;
+                arrow.transform.position = option2.transform.position;
+            }
         }
     }
 
-    // Update the arrow's position based on current selection
-    void UpdateArrowPosition()
+    // Confirm the current selection and switch to "Leave" state
+    void ConfirmSelection()
     {
-        if (selection == 0) // Claw selected
-        {
-            arrow.transform.position = Claw.transform.position; // Align the arrow with Claw
-        }
-        else if (selection == 1) // Garden selected
-        {
-            arrow.transform.position = Garden.transform.position; // Align the arrow with Garden
-        }
-    }
+        currentState = State.Leave;
 
-    // Set the active manager based on the current selection
-    void SetActiveManager()
-    {
-        if (selection == 0) // Claw selected
-        {
-            ClawManager.SetActive(true);
-            GardenManager.SetActive(false);
-        }
-        else if (selection == 1) // Garden selected
-        {
-            ClawManager.SetActive(false);
-            GardenManager.SetActive(true);
-        }
-    }
-
-    // Confirm the current selection
-    void SelectCurrentOption()
-    {
-        Debug.Log("Selected: " + currentSelection.name);
-
-        if (selection == 0) // Claw selected
-        {
-            currentSelection = Claw;
-        }
-        else if (selection == 1) // Garden selected
-        {
-            currentSelection = Garden;
-        }
-
-        // Set the appropriate manager active
-        SetActiveManager();
+        // Disable selection visuals
         arrow.SetActive(false);
-        Garden.SetActive(false);
-        Claw.SetActive(false);
+        option1.SetActive(false);
+        option2.SetActive(false);
+
+        // Activate the selected manager
+        if (selection == 0)
+        {
+            option1Manager.SetActive(true);
+        }
+        else
+        {
+            option2Manager.SetActive(true);
+        }
     }
 }
