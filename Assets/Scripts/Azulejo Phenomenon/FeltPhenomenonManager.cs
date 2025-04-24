0260f310ebee21f099de9860852a90a03ff88e2f
Antonio K. P. de Andrade <tonikpa@gmail.com>
2025-04-24 14:39:35 -0400

    adds sunflower and rat skull phenomena

--- Assets/Scripts/Azulejo Phenomenon/FeltPhenomenonManager.cs
+++ Assets/Scripts/Azulejo Phenomenon/FeltPhenomenonManager.cs
@@ -6,8 +6,30 @@ public class FeltPhenomenonManager : MonoBehaviour{
     [Header("Tanya")]
     public GameObject tanyaNPC;
 
+    [Header("Cockroach")]
+    public GameObject cockroachInteraction;
+    public float cockroachDelay = 1f;
+
+    [Header("Sunflower")]
+    public GameObject sunflowerInteraction;
+
     // Tanya Phenomenon
     public void ExecuteTanyaPhenomenon(){
         tanyaNPC.SetActive(true);
+    }
+
+    // Cockroach Phenomenon
+    public void ExecuteCockroachPhenomenon(){
+        StartCoroutine(ExecuteCockroachDelay());
+    }
+
+    private IEnumerator ExecuteCockroachDelay(){
+        yield return new WaitForSeconds(cockroachDelay);
+        cockroachInteraction.SetActive(true);
+    }
+
+    // Sunflower Interaction
+    public void ExectureSunflowerPhenomenon(){
+        sunflowerInteraction.SetActive(true);
     }
 }
