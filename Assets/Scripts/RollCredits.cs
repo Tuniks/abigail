e10de5db2e285a960a178648ef3ce738b4d84176
IsabelleSmith21 <is2812@nyu.edu>
2025-05-12 23:59:12 -0400

    credits scene + argil

--- Assets/Scripts/RollCredits.cs
+++ Assets/Scripts/RollCredits.cs
@@ -1,25 +1,57 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RollCredits : MonoBehaviour
 {
-    public RectTransform creditsText;  // Assign the RectTransform of your credits text
-    public float scrollSpeed = 30f;    // Speed of scrolling (adjust as needed)
+    public RectTransform creditsText;   // Assign the RectTransform of your credits text
+    public float scrollSpeed = 30f;     // Speed of scrolling
+    public GameObject Test;
+    public AudioSource backgroundMusic; // Assign the music AudioSource
+    public float fadeDuration = 2f;     // How long to fade out the music
+
+    public float endYPosition = 1200f;  // Set this to the Y position at which credits are fully offscreen
 
     private bool isScrolling = true;
+    private bool hasStopped = false;
 
     void Update()
     {
         if (isScrolling)
         {
             creditsText.anchoredPosition += Vector2.up * scrollSpeed * Time.deltaTime;
+
+            if (creditsText.anchoredPosition.y >= endYPosition)
+            {
+                StopCredits();
+            }
+        }
+        else if (!hasStopped)
+        {
+            hasStopped = true;
+            Test.SetActive(true);
+            StartCoroutine(FadeOutAndLoadScene());
         }
     }
 
-    // Optional: stop scrolling after a delay or position
     public void StopCredits()
     {
         isScrolling = false;
+    }
+
+    IEnumerator FadeOutAndLoadScene()
+    {
+        float startVolume = backgroundMusic.volume;
+
+        float t = 0;
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            backgroundMusic.volume = Mathf.Lerp(startVolume, 0, t / fadeDuration);
+            yield return null;
+        }
+
+        backgroundMusic.Stop();
+        SceneManager.LoadScene("START");
     }
 }
