8c6ee2cf838392c0d6f235e5ebcb3dbacafab60c
Tuniks <tonikpa@gmail.com>
2025-07-08 10:40:35 -0400

    starts branch

---
+++ Assets/Scripts/Power Azulejo/PowerManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerManager : MonoBehaviour{
+    void Start(){
+        
+    }
+
+    void Update(){
+        
+    }
+}
