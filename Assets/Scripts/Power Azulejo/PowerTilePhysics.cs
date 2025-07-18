24637222c4cf98ece25aa5321eba46813883a798
IsabelleSmith21 <is2812@nyu.edu>
2025-07-18 14:42:41 -0400

    build fix

--- Assets/Scripts/Power Azulejo/PowerTilePhysics.cs
+++ Assets/Scripts/Power Azulejo/PowerTilePhysics.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO.Compression;
 using UnityEngine;
-using UnityEngine.WSA;
 
 public class PowerTilePhysics : MonoBehaviour{
     // Tile References
