b0eae5f35f821b6ae03d1af41196a0ca65964757
IsabelleSmith21 <157531782+IsabelleSmith21@users.noreply.github.com>
2025-10-16 11:48:46 -0400

    Added new writing + order of operations for moms house

--- Assets/Scripts/Azulejo Phenomenon/MomsPhenomenonManager.cs
+++ Assets/Scripts/Azulejo Phenomenon/MomsPhenomenonManager.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
 
 public class MomsPhenomenonManager : MonoBehaviour{
     [Header("Toucan")]
     public GameObject toucanNPC;
     public AudioClip toucanTriggerSound;
+    public DialogueRunner dialogueRunner;
 
     public void ExecuteToucanPhenomenon(){
-        toucanNPC.SetActive(true);
-        PlayerInteractor.instance.GetAudioSource().PlayOneShot(toucanTriggerSound);
+
+    toucanNPC.SetActive(true);
+    PlayerInteractor.instance.GetAudioSource().PlayOneShot(toucanTriggerSound);
+
+    if (!dialogueRunner.IsDialogueRunning){
+        dialogueRunner.StartDialogue("Tuca");
+    } else {
+        Debug.LogWarning("Dialogue is already running.");
     }
+}
+
+
 }
