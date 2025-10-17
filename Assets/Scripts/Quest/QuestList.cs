33e83ac306ee8aad811a11db5c91d9f08c8dbaef
IsabelleSmith21 <157531782+IsabelleSmith21@users.noreply.github.com>
2025-10-16 23:30:03 -0400

    tried my best to get things in there

--- Assets/Scripts/Quest/QuestList.cs
+++ Assets/Scripts/Quest/QuestList.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class QuestList{
-    public Quest[] quests = new Quest[2]{
+    public Quest[] quests = new Quest[8]{
         // QUEST 1
         new Quest(
             "How to Kill One Million Tears",
@@ -53,7 +53,160 @@ public class QuestList{
                     new QuestCondition[1] {new QuestCondition(ConditionType.ShowTileTo, "Cockroach", "Gianluca")}
                 )
             }
+        ),
+        // QUEST 3
+        new Quest(
+            "The HOA is Watching You",
+            new QuestCondition[1]{
+                new QuestCondition(ConditionType.TalkTo, "Margarette"),
+
+            },
+            new QuestStep[1]{
+                new QuestStep(
+                    1,
+                    new QuestCondition[1] {new QuestCondition(ConditionType.TalkTo, "Margarette")},
+                    new QuestCondition[1] {new QuestCondition(ConditionType.Trigger, "sunflower-planted")}                  
+                )
+            }
+        ),
+           // QUEST 4
+        new Quest(
+            "Tanya's Wisdom",
+            new QuestCondition[1]{
+                new QuestCondition(ConditionType.ShowTileTo, "Butterfly", "Margarette"),
+                /*new QuestCondition(ConditionType.ShowTileTo, "Butterfly", "Ryan 1"),
+                new QuestCondition(ConditionType.ShowTileTo, "Butterfly", "Fellini"),
+                new QuestCondition(ConditionType.ShowTileTo, "Butterfly", "Gianluca"),
+                new QuestCondition(ConditionType.ShowTileTo, "Butterfly", "Timothy")*/
+            },
+            new QuestStep[3]{
+                new QuestStep(
+                   1,
+                   new QuestCondition[1] { new QuestCondition(ConditionType.ShowTileTo, "Butterfly", "Margarette") },
+                   new QuestCondition[1] {new QuestCondition(ConditionType.TalkTo, "Spider")}        
+                ),
+
+                 new QuestStep(
+                    2,
+                    new QuestCondition[1] {new QuestCondition(ConditionType.TalkTo, "Spider")},
+                    new QuestCondition[1] {new QuestCondition(ConditionType.ShowTileTo, "Gianluca's Pizzeria", "Spider")}                  
+                ),
+
+                 new QuestStep(
+                    3,
+                    new QuestCondition[1] {new QuestCondition(ConditionType.ShowTileTo, "Gianluca's Pizzeria", "Spider")},
+                    new QuestCondition[1] {new QuestCondition(ConditionType.TalkTo, "Tanya")}                  
+                )
+            },
+            QuestStatus.Active,
+            1
+        ),
+         // QUEST 5
+        new Quest(
+            "Gianluca's Prank War",
+            new QuestCondition[1]{
+                new QuestCondition(ConditionType.TalkTo, "Gianluca"),
+
+            },
+            new QuestStep[3]{
+                new QuestStep(
+                    1,
+                    new QuestCondition[1] {new QuestCondition(ConditionType.TalkTo, "Gianluca")},
+                    new QuestCondition[1] {new QuestCondition(ConditionType.Trigger, "pizza-plated")}                  
+                ),
+                new QuestStep(
+                    2,
+                    new QuestCondition[1] {new QuestCondition(ConditionType.TalkTo, "Gianluca")},
+                    new QuestCondition[1] {new QuestCondition(ConditionType.Trigger, "cockroach-released")}
+                ),
+                new QuestStep(
+                    3,
+                    new QuestCondition[1] {new QuestCondition(ConditionType.TalkTo, "Gianluca")},
+                    new QuestCondition[1] {new QuestCondition(ConditionType.Trigger, "ghost-released")}
+                )
+            }
+        ),
+
+        // QUEST 6
+        new Quest(
+            "Champions of Nostalgia",
+            new QuestCondition[1]{
+                new QuestCondition(ConditionType.TalkTo, "Timothy"),
+
+            },
+            new QuestStep[5]{
+                new QuestStep(
+                    1,
+                    new QuestCondition[1] {new QuestCondition(ConditionType.TalkTo, "Timothy")},
+                    new QuestCondition[1] {new QuestCondition(ConditionType.TalkTo, "Ryan 1")}                 
+                ),
+                new QuestStep(
+                    2,
+                    new QuestCondition[1] {new QuestCondition(ConditionType.TalkTo, "Ryan 1")},
+                    new QuestCondition[1] {new QuestCondition(ConditionType.ShowTileTo, "Caterpillar", "Ryan 1")}
+                ),
+                new QuestStep(
+                    3,
+                    new QuestCondition[1] {new QuestCondition(ConditionType.ShowTileTo, "Caterpillar", "Ryan 1")},
+                    new QuestCondition[1] {new QuestCondition(ConditionType.Trigger, "goal-scored")}
+                ),
+                new QuestStep(
+                    4,
+                    new QuestCondition[1] {new QuestCondition(ConditionType.Trigger, "goal-scored")},
+                    new QuestCondition[1] {new QuestCondition(ConditionType.ShowTileTo, "Soccer Ball", "Timothy")}
+                ),
+                new QuestStep(
+                    5,
+                    new QuestCondition[1] {new QuestCondition(ConditionType.ShowTileTo, "Soccer Ball", "Timothy")},
+                    new QuestCondition[1] {new QuestCondition(ConditionType.Trigger, "three-points-scored")}
+                )
+            }
+        ),
+
+         // QUEST 7
+        new Quest(
+            "The New Best Friend of My Old Best Friend is My Enemy",
+            new QuestCondition[1]{
+                new QuestCondition(ConditionType.Trigger, "mom-gave-at-end-of-chat"),
+
+            },
+            new QuestStep[2]{
+                new QuestStep(
+                    1,
+                    new QuestCondition[1] {new QuestCondition(ConditionType.Trigger, "mom-gave-at-end-of-chat")},
+                    new QuestCondition[1] {new QuestCondition(ConditionType.TalkTo, "Oz")}                  
+                ),
+                new QuestStep(
+                    2,
+                    new QuestCondition[1] {new QuestCondition(ConditionType.TalkTo, "Oz")},
+                    new QuestCondition[1] {new QuestCondition(ConditionType.ShowTileTo, "Bunnydeer", "Oz")}//the actual conditions will be bunnydeer and then 2/3 of the following: Stickyhand, ghost, skull
+                )
+            }
+        ),
+        // QUEST 8
+        new Quest(
+            "Brighten the House That Made You",
+            new QuestCondition[1]{
+                new QuestCondition(ConditionType.Trigger, "mom-gave-at-end-of-chat"),//could be given either at the end of of a dialogue node or after mom is shown butterfly or tuca
+
+            },
+            new QuestStep[2]{
+                new QuestStep(
+                    1,
+                    new QuestCondition[1] {new QuestCondition(ConditionType.Trigger, "mom-gave-at-end-of-chat")},
+                    new QuestCondition[1] {new QuestCondition(ConditionType.Trigger, "sunflower-fixed")}                  
+                ),
+                new QuestStep(
+                    2,
+                    new QuestCondition[1] {new QuestCondition(ConditionType.Trigger, "mom-gave-at-end-of-chat")},
+                    new QuestCondition[1] {new QuestCondition(ConditionType.TalkTo, "Tuca")}//can be triggered by phenomenon or talking to Tuca for the first time
+                )
+            }
         )
+
+
+
+
     };
 
     public QuestList(){}
